5801f214057074f930b0caa5e46ee87269841c78
Yakiv <sapa.yakiv@gmail.com>
2025-10-09 16:10:21 +0200

    feat(navbar): add logic for settings menu
    Added functions to close the settings menu on clicking outside of a container and scrolling the window.

--- settings-script.js
+++ settings-script.js
@@ -4,3 +4,15 @@ const settingsMenu = document.getElementById("settings-menu");
 settingsBtn.addEventListener("click", () => {
   settingsMenu.classList.toggle("show");
 });
+
+document.addEventListener("click", (event) => {
+  if (!settingsMenu.contains(event.target) && !settingsBtn.contains(event.target)) {
+    settingsMenu.classList.remove("show");
+  }
+})
+
+document.addEventListener("scroll", (event) => {
+  if (!settingsMenu.contains(event.target) && !settingsBtn.contains(event.target)) {
+    settingsMenu.classList.remove("show");
+  }
+})
